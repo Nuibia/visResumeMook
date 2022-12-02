0dc3566fc88221070bec5aca45502755d689debf
jiangniao <jiangniao@juwan-inc.com>
2022-12-02 15:03:27 +0800

    feat: 环境搭建完成

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,32 @@ module.exports = {
   //babel
   presets: [
     // 支持react+ts
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
   ],
   plugins: [
     // 减少冗余代码
-    "@babel/plugin-transform-runtime",
+    '@babel/plugin-transform-runtime',
     // 将es转换成commonjs
     [
-      "@babel/plugin-transform-modules-commonjs",
+      '@babel/plugin-transform-modules-commonjs',
       {
         allowTopLevelThis: true,
         loose: true,
         lazy: true,
+      },
+    ],
+    [
+      'babel-plugin-react-css-modules',
+      {
+        exclude: 'node_modules',
+        webpackHotModuleReloading: true,
+        generateScopedName: '[name]__[local]__[hash:base64:5]',
+        autoResolveMultipleImports: true,
+        filetypes: {
+          '.less': { syntax: 'postcss-less' },
+        },
       },
     ],
   ],
