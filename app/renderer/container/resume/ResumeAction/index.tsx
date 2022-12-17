12499b892ba44e7e8ba3adf32d939116b9019616
jiangniao <jiangniao@juwan.org>
2022-12-17 17:04:27 +0800

    feat: 支持导出

--- app/renderer/container/resume/ResumeAction/index.tsx
+++ app/renderer/container/resume/ResumeAction/index.tsx
@@ -1,15 +1,21 @@
 import MyButton from '@src/common/components/MyButton';
 import { ROUTER } from '@src/common/constants/router';
+import { toPrintPdf } from '@src/common/utils/htmlToPdf';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import styles from './index.less';
 
 const ResumeAction = () => {
+  const base: TSResume.Base = useSelector((state: any) => state.resumeModel.base);
+  const work: TSResume.Work = useSelector((state: any) => state.resumeModel.work);
   const history = useHistory();
   // 返回首页
   const onBack = () => history.push(ROUTER.root);
   // 导出PDF
-  const onExport = () => {};
+  const onExport = () => {
+    toPrintPdf(`${base?.username}+${base?.school}+${work?.job}`);
+  };
 
   return (
     <div className={styles.actions}>
